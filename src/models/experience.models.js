669696f7f403140a80cf24a6575cc97430a80bd0
zaki khairi <zakielbavarian@gmail.com>
2022-11-01 21:58:53 +0700

    update experience api

--- src/models/experience.models.js
+++ src/models/experience.models.js
@@ -45,9 +45,11 @@ const experienceModel = {
         });
     },
     insertExperience: (data) => {
-        const {id, position, company_name, started, ended, company_photo, description} = data
+        const {id, position, company_name, started, ended, photo, description} = data
         return new Promise ((resolve, reject) =>
-            db.query(`INSERT INTO experience (id, position, company_name, started, ended, company_photo, description) VALUES ('${id}', '${position}', '${company_name}', '${started}', '${ended}', '${company_photo}', '${description}')`, (error,result) => {
+            db.query(`
+                INSERT INTO experience (id, position, company_name, started, ended, photo, description) 
+                VALUES ('${id}', '${position}', '${company_name}', '${started}', '${ended}', '${photo}', '${description}' )`, (error,result) => {
                 if(!error){
                     resolve(result)
                 }else{
@@ -57,11 +59,11 @@ const experienceModel = {
         );
     },
     updateExperience: (data) => {
-        const { id, position, company_name, started, ended, company_photo, description } = data;
+        const { id, position, company_name, started, ended, photo, description } = data;
         return new Promise((resolve, reject) => {
             db.query(
             `UPDATE experience SET position='${position}',company_name='${company_name}', 
-            started='${started}', ended='${ended}', company_photo=${company_photo}, description='${description}'
+            started='${started}', ended='${ended}', photo=${photo}, description='${description}'
             WHERE id='${id}'`,
                 (err, res) => {
                     if (err) {
