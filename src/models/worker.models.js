fc6b4d7429f3fd15f1236dec901e62342136e9f0
zaki khairi <zakielbavarian@gmail.com>
2022-11-05 21:17:21 +0700

    undo api worker changes

--- src/models/worker.models.js
+++ src/models/worker.models.js
@@ -42,8 +42,8 @@ const workerModel = {
             portfolio.photo, portfolio.porto_description, experience.position, experience.company_name,
             experience.started, experience.ended, experience.photo, experience.exp_description 
             FROM worker 
-            RIGHT JOIN portfolio ON worker.portfolio_id = portfolio.id
-            RIGHT JOIN experience ON worker.experience_id = experience.id
+            LEFT JOIN portfolio ON worker.portfolio_id = portfolio.id
+            LEFT JOIN experience ON worker.experience_id = experience.id
             WHERE worker.id='${id}'`, (err, res) => {
                 if (err) {
                     reject(err);
